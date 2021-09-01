ae53797aa952c4d97cc4bad83c45b14bad5a9d31
Michael De Luca <35537333+deluca-mike@users.noreply.github.com>
2021-09-01 19:01:29 -0400

    feat: Complete test accounts (SC-3610) (#16)
    * feat: Complete Test Accounts
    
    * chore: PR fixes

--- contracts/test/accounts/MapleTokenUser.sol
+++ contracts/test/accounts/MapleTokenUser.sol
@@ -1,11 +1,74 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.6.11;
 
-contract MapleTokenUser {
+// TODO: odd bug not allowing IERC2222 selectors via IMapleToken
+import { IMapleToken } from "../../interfaces/IMapleToken.sol";
+import { IERC2222 }    from "../../interfaces/IERC2222.sol";
 
-    function try_permit(address mplToken, address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external returns (bool ok) {
-        string memory sig = "permit(address,address,uint256,uint256,uint8,bytes32,bytes32)";
-        (ok,) = mplToken.call(abi.encodeWithSignature(sig, owner, spender, value, deadline, v, r, s));
+import { ERC20User } from "./ERC20User.sol";
+
+contract MapleTokenUser is ERC20User {
+
+    /************************/
+    /*** Direct Functions ***/
+    /************************/
+
+    function fdt_withdrawFunds(address fdt) external {
+        IMapleToken(fdt).withdrawFunds();
+    }
+
+    function fdt_withdrawFundsOnBehalf(address fdt, address user) external {
+        IMapleToken(fdt).withdrawFundsOnBehalf(user);
+    }
+
+    function fdt_updateFundsReceived(address fdt) external {
+        IMapleToken(fdt).updateFundsReceived();
+    }
+
+    function mplToken_permit(
+        address mplToken,
+        address owner,
+        address spender,
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external
+    {
+        IMapleToken(mplToken).permit(owner, spender, amount, deadline, v, r, s);
+    }
+
+    /*********************/
+    /*** Try functions ***/
+    /*********************/
+
+    function try_fdt_withdrawFunds(address fdt) external returns (bool ok) {
+        (ok,) = fdt.call(abi.encodeWithSelector(IERC2222.withdrawFunds.selector));
+    }
+
+    function try_fdt_withdrawFundsOnBehalf(address fdt, address user) external returns (bool ok) {
+        (ok,) = fdt.call(abi.encodeWithSelector(IERC2222.withdrawFundsOnBehalf.selector, user));
+    }
+
+    function try_fdt_updateFundsReceived(address fdt) external returns (bool ok) {
+        (ok,) = fdt.call(abi.encodeWithSelector(IERC2222.updateFundsReceived.selector));
+    }
+
+    function try_permit(
+        address mplToken,
+        address owner,
+        address spender,
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external returns (bool ok)
+    {
+        (ok,) = mplToken.call(abi.encodeWithSelector(IMapleToken.permit.selector, owner, spender, amount, deadline, v, r, s));
     }
 
 }
